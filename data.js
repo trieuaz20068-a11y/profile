73c987b2e656ecef68573b0bec35c150fca61a00
trieuaz20068-a11y <trieuaz20068@gmail.com>
2025-11-08 23:03:30 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,12 @@
 // product data (images in images/ folder)
 const products = [
-  {id:1, title:'Bút gel màu 0.5', price:12000, cat:'pen', img:'images/butgelmau0.5.jpeg', desc:'Bút gel êm, mực lên đều, thích hợp viết nhanh.'},
-  {id:2, title:'Bút bi xịn', price:8000, cat:'pen', img:'images/bbx.jpg', desc:'Bút bi siêu bền, viết mượt, mẫu mã đẹp.'},
-  {id:3, title:'Tập 200 trang', price:18000, cat:'notebook', img:'images/s200.png', desc:'Tập 200 trang, giấy trắng, bìa cứng.'},
-  {id:4, title:'Tập kẻ ngang', price:15000, cat:'notebook', img:'images/tkn.jpeg', desc:'Tập kẻ ngang A4, phù hợp học sinh.'},
-  {id:5, title:'Balo trẻ em', price:250000, cat:'bag', img:'images/balo.webp', desc:'Balo nhẹ, nhiều ngăn, họa tiết dễ thương.'},
-  {id:6, title:'Hộp bút vẽ', price:45000, cat:'pen', img:'images/hopbutve.jpeg', desc:'Hộp bút đủ màu, chất lượng tốt cho học vẽ.'},
-  {id:7, title:'Sổ tay A5', price:22000, cat:'notebook', img:'images/sta5.jpg', desc:'Sổ tay tiện dụng A5, giấy mịn.'},
-  {id:8, title:'Túi đựng bút', price:12000, cat:'bag', img:'images/tdb.svg', desc:'Túi đựng bút nhỏ gọn, dễ mang theo.'}
+  {id:1, title:'Bút gel màu 0.5', price:12000, cat:'pen', img:'butgelmau0.5.jpeg', desc:'Bút gel êm, mực lên đều, thích hợp viết nhanh.'},
+  {id:2, title:'Bút bi xịn', price:8000, cat:'pen', img:'bbx.jpg', desc:'Bút bi siêu bền, viết mượt, mẫu mã đẹp.'},
+  {id:3, title:'Tập 200 trang', price:18000, cat:'notebook', img:'s200.png', desc:'Tập 200 trang, giấy trắng, bìa cứng.'},
+  {id:4, title:'Tập kẻ ngang', price:15000, cat:'notebook', img:'tkn.jpeg', desc:'Tập kẻ ngang A4, phù hợp học sinh.'},
+  {id:5, title:'Balo trẻ em', price:250000, cat:'bag', img:'balo.webp', desc:'Balo nhẹ, nhiều ngăn, họa tiết dễ thương.'},
+  {id:6, title:'Hộp bút vẽ', price:45000, cat:'pen', img:'hopbutve.jpeg', desc:'Hộp bút đủ màu, chất lượng tốt cho học vẽ.'},
+  {id:7, title:'Sổ tay A5', price:22000, cat:'notebook', img:'sta5.jpg', desc:'Sổ tay tiện dụng A5, giấy mịn.'},
+  {id:8, title:'Túi đựng bút', price:12000, cat:'bag', img:'tdb.svg', desc:'Túi đựng bút nhỏ gọn, dễ mang theo.'}
 ]
+
